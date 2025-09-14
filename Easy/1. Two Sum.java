4fa609512cad6979a49c05c0558290503ceea898
Peddireddy Tejaswini <tejaswinipeddireddy16@gmail.com>
2025-09-14 12:22:33 +0530

    Add files via upload

---
+++ Easy/1. Two Sum.java
@@ -0,0 +1,20 @@
+//  1. Two Sum
+// Given an array of integers nums and an integer target, 
+// return indices of the two numbers such that they add up to target.
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        int n = nums.length;
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};  
+                }
+            }
+        }
+        
+        return new int[]{}; 
+    }
+}
